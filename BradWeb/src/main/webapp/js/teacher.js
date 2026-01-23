0a613f892e79c8c7b62a67aa8d8cb7ae82eb0869
Tony191911 <fentony5@gmail.com>
2026-01-23 15:56:41 +0800

    白板

---
+++ BradWeb/src/main/webapp/js/teacher.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+	let clear = document.getElementById("clear");
+	let myDrawer= document.getElementById("myDrawer");
+	let webSocket = new WebSocket("ws://10.0.101.133:8080/BradWeb/mycenter");
+	let isConnect = false;
+	
+		
+	webSocket.onopen = function(){
+		isConnect = true;
+		webSocket.send(JSON.stringify({isTeacher:true}))
+	}
+	webSocket.onclose = function(){
+		isConnect = false;
+	}
+	webSocket.onerror = function(){
+		console.log("onError" + event);
+	}
+	
+	//-------------------------------------------------
+	let ctx = myDrawer.getContext("2d");
+	let isDrag = false;
+	myDrawer.onmousedown = function(e){
+		isDrag = true;
+		let x = e.offsetX, y = e.offsetY;
+		ctx.beginPath();
+		ctx.lineWidth = 4;
+		ctx.moveTo(x, y);
+	}
+	myDrawer.onmouseup = function(e){
+		isDrag = false;
+	}
+	myDrawer.onmousemove = function(e){
+		if (isDrag) {
+			let x = e.offsetX, y = e.offsetY;
+			ctx.lineTo(x, y);
+			ctx.stroke();
+		}
+	}
+}
+	
