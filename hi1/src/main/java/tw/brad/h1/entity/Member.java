7e090556c7c0ab0990903c6e1ba2663e910fe546
Tony191911 <fentony5@gmail.com>
2026-02-02 15:33:43 +0800

    02021533

--- hi1/src/main/java/tw/brad/h1/entity/Member.java
+++ hi1/src/main/java/tw/brad/h1/entity/Member.java
@@ -26,6 +26,10 @@ public class Member {
 	
 	@Column(name = "icon")
 	private byte[] icon;
+	
+	public Member(long id, String email, String name) {
+		this.id = id; this.email = email; this.name = name;
+	}
 
 	public long getId() {
 		return id;
