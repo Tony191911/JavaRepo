04741c127ae0c073625a4f18e8a14f4939747bd0
Tony191911 <fentony5@gmail.com>
2026-02-05 09:21:39 +0800

    02050921

--- hi1/src/main/java/tw/brad/h1/tutor/Brad16.java
+++ hi1/src/main/java/tw/brad/h1/tutor/Brad16.java
@@ -2,6 +2,7 @@ package tw.brad.h1.tutor;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import tw.brad.h1.dao.SCDao;
 import tw.brad.h1.entity.Course;
@@ -19,17 +20,28 @@ public class Brad16 {
 		s1 = dao.getStudentById(sid);
 		System.out.printf("Welcome, %s", s1.getSname());
 		
-		List<Course> courses = dao.getAllCourse();
-		for (Course course : courses) {
-			System.out.printf("%d. %s\n", course.getId(), course.getCname());
+		while (true) {
+			List<Course> courses = dao.getAllCourse();
+			for (Course course : courses) {
+				if (!isExist(s1, course.getStudents())) {
+					System.out.printf("%d. %s\n", course.getId(), course.getCname());
+				}
+			}
+			
+			System.out.println("------");
+			System.out.println("Which? (0: for Exit):");
+			long cid = scanner.nextLong();
+			
+			if (cid <= 0) break;
+			
+			s1.addCourse(dao.getCourseById(cid));
+			s1 = dao.update(s1);
 		}
-		System.out.println("------");
-		System.out.println("Which?");
-		long cid = scanner.nextLong();
-		
-		s1.addCourse(dao.getCourseById(cid));
-		dao.update(s1);
-		
+	}
+	
+	static boolean isExist(Student s, Set<Student> students) {
+		return students.stream()
+				.anyMatch(student -> student.getId().equals(s.getId()));
 	}
 
 }
