0e0ac55455c7ebfb8457d5016cc3f1ad7d26197e
Tony191911 <fentony5@gmail.com>
2026-02-09 16:33:01 +0800

    02091632

---
+++ Spring01/src/main/java/com/example/spring1/dto/Order.java
@@ -0,0 +1,36 @@
+package com.example.spring1.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private int orderId;
+    private String orderDate;
+    private List<OrderDetail> details = new ArrayList<>();
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<OrderDetail> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<OrderDetail> details) {
+        this.details = details;
+    }
+}
