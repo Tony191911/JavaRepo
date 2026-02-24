0aae7f1a90137bba6b25c359bdef206a2f29bd4f
Tony191911 <fentony5@gmail.com>
2026-02-24 15:58:45 +0800

    02241558

---
+++ Spring07/src/main/resources/static/js/teacher.js
@@ -0,0 +1,77 @@
+window.onload = function() {
+	let clear = document.getElementById("clear");
+	let myDrawer= document.getElementById("myDrawer");
+	let webSocket = new WebSocket("ws://10.0.101.133:8080/ws");
+	let isConnect = false;
+	
+	// 連線成功後，告訴server(/mycenter)我是老師
+	webSocket.onopen = function(){
+		isConnect = true;
+		webSocket.send(JSON.stringify({isTeacher:true}))
+	}
+	// 關閉連線
+	webSocket.onclose = function(){
+		isConnect = false;
+	}
+	// 印出錯誤
+	webSocket.onerror = function(){
+		console.log("onError" + event);
+	}
+	
+	//-------------------------------------------------
+	
+	// 取得畫圖工具
+	let ctx = myDrawer.getContext("2d");
+	let isDrag = false;
+	// 滑鼠點下去開始畫線
+	myDrawer.onmousedown = function(e){
+		isDrag = true;
+		// 取得滑鼠在canvas上的x, y座標
+		let x = e.offsetX, y = e.offsetY;
+		// 清空舊的畫筆路徑，重新開始畫一條新的路徑
+		// 把畫筆放到(x,y)上，起點
+		ctx.beginPath();
+		ctx.lineWidth = 4;
+		ctx.moveTo(x, y);
+		
+		let data = {
+			isClear : false,
+			isNewLine : true, 
+			x : x,
+			y : y
+		};
+		webSocket.send(JSON.stringify(data));
+		
+	}
+	// 滑鼠放開停止畫線
+	myDrawer.onmouseup = function(e){
+		isDrag = false;
+	}
+	// 滑鼠移動持續畫線
+	myDrawer.onmousemove = function(e){
+		if (isDrag) {
+			let x = e.offsetX, y = e.offsetY;
+			// 定義線段終點
+			// 把線顯示到canvas上
+			ctx.lineTo(x, y);
+			ctx.stroke();
+			
+			let data = {
+				isClear : false,
+				isNewLine : false, 
+				x : x,
+				y : y
+			};
+			webSocket.send(JSON.stringify(data));
+		}
+	}
+	// 清除畫布
+	clear.addEventListener("click", function(){
+		ctx.clearRect(0, 0, myDrawer.width, myDrawer.height);
+		let data = {
+			isClear : true
+		};
+		webSocket.send(JSON.stringify(data));
+	});
+}
+	
